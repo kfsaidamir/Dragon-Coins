3aca2bcbfa223455009164a6ef5e8d69a917724d
kf_saidamir <skadirov2009@gmail.com>
2024-07-22 13:00:11 +0500

    A

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraBaseProvider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </ChakraBaseProvider>
   </React.StrictMode>
 );
